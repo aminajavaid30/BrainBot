c07f09fd9ef56e6ec3ae7a6bfc87bc19cb3de564
Amina Javaid <103302290+aminajavaid30@users.noreply.github.com>
2024-05-13 12:21:12 +0500

    Update BrainBot.py

--- BrainBot.py
+++ BrainBot.py
@@ -50,6 +50,7 @@ def save_uploaded_image(uploaded_image):
 def save_uploaded_file_on_server(uploaded_file):
     # POST request to FastAPI endpoint
     response = requests.post("https://huggingface.co/spaces/aminaj/BrainBot/upload_file_on_server", files={"file": uploaded_file})
+    st.write(response)
     temp_file_path = response.filename
     return temp_file_path
 
